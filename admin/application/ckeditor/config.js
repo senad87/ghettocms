169ef14cc3fcb0ca9728604ed7090eff8fde5fbd
Senad Mehic <hladnopivo87@gmail.com>
2012-05-31 20:30:28 +0200

    ckeditor plugin

--- admin/application/ckeditor/config.js
+++ admin/application/ckeditor/config.js
@@ -1,4 +1,4 @@
-﻿/*
+/*
 Copyright (c) 2003-2010, CKSource - Frederico Knabben. All rights reserved.
 For licensing, see LICENSE.html or http://ckeditor.com/license
 */
@@ -18,9 +18,11 @@ config.toolbar_Full =
     ['NumberedList','BulletedList'],
     ['JustifyLeft','JustifyCenter','JustifyRight','JustifyBlock'],
     ['Link','Unlink','Anchor'],
-    ['Image','Table','HorizontalRule','SpecialChar'],
+    ['Image','Table','HorizontalRule','SpecialChar', 'Gallery'],
     
 ];
  
+config.extraPlugins = 'gallery'; //e.g 'plugin1, plugn2, plugin3'
+
 
 };
