0735509e68305e4bd8a0a3534bac66a8cfa467dc
Senad Mehic <hladnopivo87@gmail.com>
2012-07-21 14:53:47 +0200

    sredjivanje gita

---
+++ admin/application/helpers/paths_helper.php
@@ -0,0 +1,5 @@
+<?php
+
+function root_url(){
+    return "http://localhost/ghettocms/";
+}
---
+++ admin/application/style/js/config.js
@@ -0,0 +1,7 @@
+
+var config = { 
+    base_url: "http://localhost/ghettocms/admin/",
+    frontend_url: "http://localhost/ghettocms/" 
+}
+
+
