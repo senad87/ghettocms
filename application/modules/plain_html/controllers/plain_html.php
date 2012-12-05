d24f1b1cc21904c40a4da3c55baa440a664fcac5
damir <mehicdado@gmail.com>
2012-12-05 22:31:27 +0100

    fix articles, menus and plain_html params loading

--- application/modules/plain_html/controllers/plain_html.php
+++ application/modules/plain_html/controllers/plain_html.php
@@ -16,20 +16,8 @@ class Plain_html extends MX_Controller {
 	function displayme($module_id, $data=array()){
 		$module_instance = $this->Position_model->get_module_by_id($module_id);
 		//load module params
-		$params_data_array = explode(";;",$module_instance[0]->params);
-		$module_params = array();
-		foreach ($params_data_array as $params_data){
-			$param_data = explode(":=", $params_data);
-			/*if($param_data[0] == "categories"){
-				$param_data[1] = explode(",",$param_data[1]);
-				//$module_params[$param_data[0]] = $param_data[1];
-			}*/
-				
-			$module_params[$param_data[0]] = $param_data[1];
-				
-		}
-		//var_dump($module_params['html_source']);
-		//$data['plain_html'] = $module_params['html_source'];
+                $module_params = unserialize( $module_instance[0]->params );
+		
 		include $this->load->_ci_model_paths[0].'views/plain_html_view.php';
 		//$this->load->view("plain_html_view", $data);
 	}
