fe454f443d68ba7b2233db831644995d21537a1e
Ubuntu 12.04 LTS <damir@ubuntu.(none)>
2012-12-11 17:10:59 +0100

    all html comments removed from homepage template

--- application/views/digithome_view.php
+++ application/views/digithome_view.php
@@ -23,30 +23,8 @@
    	<div class="menuContainer">
          <div class="logo"><a href="<?php echo base_url();?>"><img src="<?php echo base_url();?>application/views/images/logo.png" width="306" height="110" /></a></div>
          <div class="menuBox">
-             <?php echo modules::run('position', 1, $menu[0]->id); ?>
-            <!-- <ul class="menu">
-               <li>
-               <a href="3">Šta je digitalna tv?</a>
-               </li>
-               <li>
-               <a href="#">Digitalna tv u srbiji</a>
-               </li>
-               <li>
-               <a href="#">Česta pitanja</a>
-               </li>
-               <li>
-               <a href="#">Kontakt</a>
-               </li>
-            </ul>   -->      
-         
+         <?php echo modules::run('position', 1, $menu[0]->id); ?>
          <?php echo modules::run('position', 2, $menu[0]->id); ?>
-         <!-- <div class="forma-pretraga">
-         <form method="post" action="">
-                  <input type="text" value="" class="pretraga" name="keyword">
-                  <input type="hidden" value="441" name="id">
-                  <input type="submit" class="button" value="">
-         </form>        
-         </div> -->
       </div>
       
       
@@ -54,54 +32,9 @@
    <div class="clear"></div>
 <div class="container">
      <?php echo modules::run('position', 3, $menu[0]->id); ?> 
-<!-- <div class="headboxContainer">
-			<div class="headbox">
-
-
-
-<div id="headboxSlider" class="royalSlider heroSlider rsMinW">
-  <div class="rsContent">
-    <img class="rsImg" src="<?php echo base_url();?>application/views/images/slider/4.jpg" alt="" />
-    <div class="infoBlock infoBlockLeftBlack rsABlock rsNoDrag" data-fade-effect="" data-move-offset="10" data-move-effect="bottom" data-speed="200">
-       <h4>Da li se vidimo?</h4>
-      <p>Potražite svoj grad na listi, i pogledajte koji predajnik emituje koje kanale za vaš grad.</p>
-      
- 	</div>
- 	<div data-delay="350" class="bigButton rsNoDrag rsAbsoluteEl" data-fade-effect="" data-move-offset="10" data-move-effect="bottom" data-speed="200"><a href="#">Više informacija</a></div>
-   </div>
-   
-   
-   
-  <div class="rsContent">
-    <img class="rsImg" src="<?php echo base_url();?>application/views/images/slider/2.jpg" alt="" />
-    <div class="infoBlock rsABlock infoBlockLeftBlack rsNoDrag" data-fade-effect="" data-move-offset="10" data-move-effect="bottom" data-speed="200">
-      <h4>Touch events are disabled over this text to allow selection</h4>
-      <p>You can add this behaviour to any element inside of slide just by adding 'rsNoDrag' CSS class to it.</p>
-    </div>
-  </div>
- <div class="rsContent">
-    <img class="rsImg" src="<?php echo base_url();?>application/views/images/slider/3.jpg" alt="" />
-    <div class="infoBlock rsABlock infoBlockLeftBlack rsNoDrag" data-fade-effect="" data-move-offset="10" data-move-effect="bottom" data-speed="200">
-      <h4>Touch events are disabled over this text to allow selection</h4>
-      <p>You can add this behaviour to any element inside of slide just by adding 'rsNoDrag' CSS class to it.</p>
-    </div>
-  </div>
-</div>
-
-
-
-<img src="<?php echo base_url();?>application/views/images/slider-linija.png" width="960" height="127" style="position: absolute; bottom: 0; z-index: 110;" />
-
-
-</div>
-         <div class="headboxBottom">Potpuno gašenje analognog servisa izvršiće se do 17. juna 2015. godine.<br />
-			<strong>Da li ste spremni za prelazak na digitalnu televiziju?</strong>
-         
-         </div>
-      </div> -->
       
    <!-- Box -->
-      <div class="roundedBox">
+   <div class="roundedBox">
         <div class="threeImages">
             <?php echo modules::run('position', 4, $menu[0]->id); ?>
             <?php echo modules::run('position', 5, $menu[0]->id); ?>
@@ -111,22 +44,11 @@
          <?php echo modules::run('position', 8, $menu[0]->id); ?>    
       <div class="clear"></div>
       <?php echo modules::run('position', 9, $menu[0]->id); ?>
-      <!-- <div class="stationList">
-      
-      	<h5>TV stanice čiji programi mogu da se prate u Inicijalnoj mreži</h5>
-         <img src="<?php echo base_url();?>application/views/images/stanice.jpg" width="880" height="196" />
-         
-         </div> -->
       
          <div class="footer">
          Copyright © 2012. Javno preduzeće "Emisiona tehnika i veze" Beograd
          <span class="pw">Produkcija <a href="http://www.amedia.co.rs" target="_blank">A-Media d.o.o.</a></span>
-         </div>
-      
-      
-      
-      
-      
+         </div>    
    </div> 
          
       <!--Box end -->
