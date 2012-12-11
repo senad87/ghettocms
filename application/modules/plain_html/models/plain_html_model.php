632a60699151edb3acaecd2739bdbe6c1ad50bf9
Ubuntu 12.04 LTS <damir@ubuntu.(none)>
2012-12-12 00:18:55 +0100

    model folder added for plain_html module

---
+++ application/modules/plain_html/models/plain_html_model.php
@@ -0,0 +1,11 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Plain_html_model extends CI_Model {
+
+	function __construct()
+	{
+		parent::__construct();	
+	}
+        
+        
+}
