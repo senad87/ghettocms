8c3f4cc4dee82b8f04452e31a414b40111898e95
Manson <milan@ghetto.rs>
2012-12-11 17:06:44 +0100

    milan story list desing fix

--- application/modules/articles/views/article_list_view.php
+++ application/modules/articles/views/article_list_view.php
@@ -1,14 +1,22 @@
-<div id="<?php echo $module_id; ?>" class="roundedBox4 <?php if (isset($module_params['classsuffix']) && $module_params['classsuffix'] != "") echo $module_params['classsuffix']; ?>">
-    <div class="contentTitleZone">  
-        <h1 class="title"><?php
+ <div class="newsList <?php if (isset($module_params['classsuffix']) && $module_params['classsuffix'] != "") echo $module_params['classsuffix']; ?>"> 
+        <h2 class="title"><?php
             if ($module_params['box_title'] != "") {
                 echo $module_params['box_title'];
             } ?>
-        </h1>
-        <p>Najnovije vesti vezane za prelazak na digitalnu televiziju</p>
-    </div>
+            
+            
+                        <?php if ( $module_params['readmore'] >= $a ) { ?>
+                            <a class="smallButton" href="<?php echo base_url(); ?><?php echo $story['type_name']; ?>/<?php echo $data['menu_id']; ?>/<?php echo $story['id']; ?>/<?php echo url_title($story['title']); ?>">Pogledaj sve</a>
+                        <?php } ?>            
+            
+            
+        
+        
+        
+        </h2>
+        
     
-    <div class="contentList">
+
         <ul>
         <?php
         //counter of the displayed articles
@@ -23,69 +31,41 @@
                 </a>    
                 <?php } ?>
             <?php } ?>                
-            <div class="newsItem">
+           
                 <?php if ($module_params['title'] >= $a) { ?>
-                    <div class="title">
-                        <a href="<?php echo base_url(); ?><?php echo $story['type_name']; ?>/<?php echo $data['menu_id']; ?>/<?php echo $story['id']; ?>/<?php echo url_title($story['title']); ?>">
+                    <span class="qm"></span>
+                    <div class="qList">
+                        <a class="link" href="<?php echo base_url(); ?><?php echo $story['type_name']; ?>/<?php echo $data['menu_id']; ?>/<?php echo $story['id']; ?>/<?php echo url_title($story['title']); ?>">
                             <?php echo $story['title']; ?>
                         </a>
-                    </div>
+                   </div>
                 <?php } ?>
                 <!-- Creation Date -->    
                 <?php if ($module_params['creation_date'] >= $a) { ?>
-                <div class="date">
+                <span>
                     <?php echo srb_date($story['creation_date']); ?>
-                </div>    
+                </span>    
                 <?php } ?>
                 <!-- end of Creation Date -->
                 <!-- Lead -->
                 <?php if ( $module_params['lead'] >= $a && $story['lead'] ) { ?>
                     <div class="lead">
                         <?php echo $story['lead']; ?><br />
-
-                        <?php if ( $module_params['readmore'] >= $a ) { ?>
-                            <a href="<?php echo base_url(); ?><?php echo $story['type_name']; ?>/<?php echo $data['menu_id']; ?>/<?php echo $story['id']; ?>/<?php echo url_title($story['title']); ?>">Opširnije ></a>
-                        <?php } ?>
                     </div>
                 <?php } ?>
                 <!-- end of Lead -->    
                 <div class="clear"></div>
-            </div><!-- end of newsItem -->
+           
       </li>
       <!-- end of one news Item -->
-        <!-- NO NEED TO USE THIS FOR DIGITALIZACIJA            
-       <div class="news-block">  
-       <div class="news-block-info">
-       <?php if ($module_params['author'] >= $a) { ?>
-       Postavio: <a href="#"><?php echo $story['author_name']; ?></a>
-       <?php } ?>                
-       <span class="separator">&nbsp;</span>
-       <span class="separator">&nbsp;</span>
-       <?php if ($module_params['category_name'] >= $a && $story['menu_name']) { ?>
-       <a href="<?php echo base_url(); ?><?php echo $story['menu_name']->name; ?>/<?php echo $story['category']->menu_id; ?>/"><?php echo $story['menu_name']->name; ?></a>
-       <?php } ?>
-       <span class="separator">&nbsp;</span>
-       <?php if ($module_params['number_of_comments'] >= $a) { ?>
-       <a href="<?php echo base_url(); ?><?php echo $story['type_name']; ?>/<?php echo $data['menu_id']; ?>/<?php echo $story['id']; ?>/<?php echo url_title($story['title']); ?>#commlist"><?php echo count_comments($story['id']); ?> komentara</a>
-       <?php } ?>                  
-       </div>
-       <?php if ($module_params['readmore'] >= $a) { ?>
-       <div class="readmore">
-       <?php if ($story['tag_id'] > 0) { ?>
-       <p class="tags2">
-       <a href="<?php echo base_url(); ?>tags/index/<?php echo $data['menu_id']; ?>/<?php echo $story['tag_id']; ?>"><?php echo $story['tag']; ?></a>
-       </p>
-       <?php } ?>
-       <a href="<?php echo base_url(); ?><?php echo $story['type_name']; ?>/<?php echo $data['menu_id']; ?>/<?php echo $story['id']; ?>/<?php echo url_title($story['title']); ?>">Opširnije ></a>
-       </div>
-           <?php } ?>              
-       </div> -->
+   
 
 <?php
 $a++;
 }
 ?>
 </ul>
+
             <!-- <div><?php echo $pagination; ?></div> -->
                    <!-- <?php if ($module_params['link_title'] != "") { ?>
                    <div class="mainlink"><a href="<?php echo base_url(); ?><?php
@@ -95,12 +75,12 @@ $a++;
                                         ?>" /> <?php echo $module_params['link_title']; ?> > </a></div>
              <?php } ?> -->
 <?php if ( $total_rows > $module_params['number'] ) { ?>
-    <div class="pagination<?php if (isset($module_params['classsuffix']) && $module_params['classsuffix'] != "") echo $module_params['classsuffix']; ?>">
+    <div class="pagination <?php if (isset($module_params['classsuffix']) && $module_params['classsuffix'] != "") echo $module_params['classsuffix']; ?>">
         <?php echo $pagination; ?>
     </div>
 <?php } ?>   
-    </div> <!-- end of content list -->
-</div>  <!-- end of roundedBox4 --> 
+
+</div>
 <script>
     $('html, body').animate({ scrollTop: $('.body-wrapper').offset().top }, 'slow');
 </script>    
