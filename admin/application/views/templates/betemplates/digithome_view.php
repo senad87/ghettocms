b929a2cb4dd02070affe542bef6399b69adb4c67
Manson <milan@ghetto.rs>
2012-12-11 15:27:09 +0100

    milan changes fe home page

--- admin/application/views/templates/betemplates/digithome_view.php
+++ admin/application/views/templates/betemplates/digithome_view.php
@@ -16,61 +16,101 @@
 
 .header-box {
 	padding: 10px;
+	float: right;
+	width: 700px;
 }
 
 .banner-top {
-	height: 150px;
+	height: 120px;
 	background-color: #6CF;
-}
-
-.left-box {
-	width: 67%;
-	float: left;
-	padding: 10px 0 10px 10px;
-
-}
-
-.right-box {
-	width: 27%;
 	float: right;
+	width: 600px;
+}
+
+.search {
+	height: 120px;
+	background-color: #6CF;
+	float: right;
+	width: 600px;
+	margin-top: 10px;
+
+}
+
+
+.headbox {
+	padding: 10px 0 10px 10px;
+}
+
+.three-banners {
 	padding: 10px 10px 10px 0;
 }
 
-
-.story-list {
-	height: 400px;
+.three-banners .banner {
+	float: left;
+	width: 30%;
+	height: 150px;
 	background-color: #6CF;
+	margin: 0 18px;
+}
+
+.hbox {
+	height: 300px;
+	background-color: #6CF;
+}
+
+.lists .news_list {
+	float: left;
+	width: 46%;
+	height: 150px;
+	background-color: #6CF;
+	margin: 0 18px;
 }
 
 
 .module-box {
 	margin-bottom: 10px;
-	background-color: #6CF;
+	padding: 10px;
 }
+
+.footer {
+	background-color: #6CF;
+	padding: 10px 10px 10px 0;	
+	}
+
+
+
 </style>
 <div class="header-box">
 	<div id="position1" class="banner-top"><input class="add-module" id="1" type="button" value="+ Add Module" />
 	</div>
-</div>
-<div class="left-box">
-<div id="position2" class="story-list"><input class="add-module" id="2" type="button" value="+ Add Module" />
+	<div id="position2" class="search"><input class="add-module" id="2" type="button" value="+ Add Module" />
+	</div>   
+   <div class="clear"></div>
+</div>   <div class="clear"></div>
+<div class="headbox">
+<div id="position3" class="hbox"><input class="add-module" id="3" type="button" value="+ Add Module" />
 </div>    
 </div>
 
-<div class="right-box">
-	<div id="position3" class="module-box"><input class="add-module" id="3" type="button" value="+ Add Module" />
+<div class="three-banners">
+	<div id="position4" class="banner"><input class="add-module" id="4" type="button" value="+ Add Module" />
 	</div>
-	<div id="position4" class="module-box"><input class="add-module" id="4" type="button" value="+ Add Module" />
+	<div id="position5" class="banner"><input class="add-module" id="5" type="button" value="+ Add Module" />
 	</div>
-	<div id="position5" class="module-box"><input class="add-module" id="5" type="button" value="+ Add Module" />
+	<div id="position6" class="banner"><input class="add-module" id="6" type="button" value="+ Add Module" />
 	</div>
-	<div id="position6" class="module-box"><input class="add-module" id="6" type="button" value="+ Add Module" />
-	</div>
-<div id="position7" class="module-box"><input class="add-module" id="7" type="button" value="+ Add Module" />
-	</div>
-<div id="position8" class="module-box"><input class="add-module" id="8" type="button" value="+ Add Module" />
-	</div>
-<div id="position9" class="module-box"><input class="add-module" id="9" type="button" value="+ Add Module" />
-	</div>
-</div>
 <div class="clear"></div>
+</div>
+
+<div class="lists">
+	<div id="position7" class="news_list"><input class="add-module" id="7" type="button" value="+ Add Module" />
+	</div>
+	<div id="position8" class="news_list"><input class="add-module" id="8" type="button" value="+ Add Module" />
+	</div>
+<div class="clear"></div>
+</div>
+<div class="module-box">
+	<div id="position9" class="footer"><input class="add-module" id="9" type="button" value="+ Add Module" />
+   </div>
+</div>
+
