6b5f97c61d83da96f109d5ff1483ce7b3347fa6f
Ubuntu 12.04 LTS <damir@ubuntu.(none)>
2012-12-12 15:53:44 +0100

    front rnd template for article list added

---
+++ application/views/digitlist_view.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Digitalizacija</title>
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:300,400,600&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+<link href="<?php echo base_url();?>application/views/css/style.css" rel="stylesheet" type="text/css" />
+    <!--[if IE 8]>
+    <link rel="stylesheet" type="text/css" media="all" href="css/ie8style.css" />
+    <![endif]-->
+    <!--[if gte IE 9]>
+    <link rel="stylesheet" type="text/css" media="all" href="css/ie9style.css" />
+    <![endif]-->
+    
+    
+    <link href="<?php echo base_url();?>application/views/css/slider.css" rel="stylesheet" />
+    <script src="<?php echo base_url();?>application/views/js/jquery-1.7.2.min.js"></script>      
+    <script src="<?php echo base_url();?>application/views/js/slider.min.js"></script>    
+    <link href="<?php echo base_url();?>application/views/css/slider.css" rel="stylesheet" />
+    
+    
+
+</head>
+<body>
+   <div class="header">
+   	<div class="menuContainer">
+         <div class="logo"><a href="<?php echo base_url();?>"><img src="<?php echo base_url();?>application/views/images/logo.png" width="306" height="110" /></a></div>
+         <div class="menuBox">
+         <?php echo modules::run('position', 1, $menu[0]->id); ?>
+         <?php echo modules::run('position', 2, $menu[0]->id); ?>
+      </div>
+         
+   <div class="clear"></div>
+<div class="container">
+<?php echo modules::run('position', 3, $menu[0]->id); ?>      
+<!-- <div class="contentHeader">
+   <img src="<?php echo base_url();?>application/views/images/header-page.jpg" width="960" height="125" />
+</div> -->
+      
+<!-- Box -->
+      <div class="roundedBox4">
+      	<?php echo modules::run('position', 4, $menu[0]->id); ?>
+         <!-- <div class="contentTitleZone">
+         	<h1>AKTUELNOSTI</h1>
+            <p>Najnovije vesti vezane za prelazak na digitalnu televiziju</p>
+         </div> -->
+      	<?php echo modules::run('position', 5, $menu[0]->id); ?>
+         <!-- <div class="contentList">
+         
+         	<ul>
+            	<li> <a href="#"><img src="<?php echo base_url();?>application/views/images/temp6.jpg" width="300" height="169" /></a>
+               <div class="newsItem">
+               	<div class="title"><a href="#">Ostvareno više od 15% procesa digitalizacije</a></div>
+                  <div class="date">31.12.2012.</div>
+                  <div class="lead">Prelazak sa analognog na digitalno emitovanje televizijskog programa kasni i za šest godina je ostvareno svega 15 odsto tog procesa, izjavio je državni sekretar u Ministarstvu trgovine i …
+                  
+                  <br />
+						<a href="#">Opširnije » </a></div>
+               
+               </div>
+               <div class="clear"></div> 
+               </li>
+               
+               
+               
+               <li> <a href="#"><img src="images/temp6.jpg" width="300" height="169" /></a>
+               <div class="newsItem">
+               	<div class="title"><a href="#">Ostvareno više od 15% procesa digitalizacije</a></div>
+                  <div class="date">31.12.2012.</div>
+                  <div class="lead">Prelazak sa analognog na digitalno emitovanje televizijskog programa kasni i za šest godina je ostvareno svega 15 odsto tog procesa, izjavio je državni sekretar u Ministarstvu trgovine i …
+                  
+                  <br />
+						<a href="#">Opširnije » </a></div>
+               
+               </div>
+               <div class="clear"></div> 
+               </li>
+               
+               
+               
+               <li> <a href="#"><img src="images/temp6.jpg" width="300" height="169" /></a>
+               <div class="newsItem">
+               	<div class="title"><a href="#">Ostvareno više od 15% procesa digitalizacije</a></div>
+                  <div class="date">31.12.2012.</div>
+                  <div class="lead">Prelazak sa analognog na digitalno emitovanje televizijskog programa kasni i za šest godina je ostvareno svega 15 odsto tog procesa, izjavio je državni sekretar u Ministarstvu trgovine i …
+                  
+                  <br />
+						<a href="#">Opširnije » </a></div>
+               
+               </div>
+               <div class="clear"></div> 
+               </li>
+               
+               
+               
+               <li> <a href="#"><img src="images/temp6.jpg" width="300" height="169" /></a>
+               <div class="newsItem">
+               	<div class="title"><a href="#">Ostvareno više od 15% procesa digitalizacije</a></div>
+                  <div class="date">31.12.2012.</div>
+                  <div class="lead">Prelazak sa analognog na digitalno emitovanje televizijskog programa kasni i za šest godina je ostvareno svega 15 odsto tog procesa, izjavio je državni sekretar u Ministarstvu trgovine i …
+                  
+                  <br />
+						<a href="#">Opširnije » </a></div>
+               
+               </div>
+               <div class="clear"></div> 
+               </li>                              
+               
+               
+         	</ul>
+         
+                <div class="pagination">
+                        <ul class="pagination">
+                        <li class="current_page">1</li>
+                      <li><a href="#">2</a></li>
+                      <li><a href="#">3</a></li>
+                      <li class="next_page"><a href="#">&gt;</a></li>
+                      <li class="last_page"><a href="#">&gt;&gt;</a></li>
+                        </ul>
+                </div>
+
+
+  
+            
+            
+            
+           
+  </div>--><!-- end of contentList -->
+         
+      	
+         <div class="footer">
+         Copyright © 2012. Javno preduzeće "Emisiona tehnika i veze" Beograd
+         <span class="pw">Produkcija <a href="http://www.amedia.co.rs" target="_blank">A-Media d.o.o.</a></span>
+         </div>
+      
+      
+      
+      
+      
+   </div> 
+         
+      <!--Box end -->
+      
+      
+</div>
+
+ 
+
+
+</body>
+</html>
