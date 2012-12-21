4240d1438879513e0e6fa5bf3054800975545b67
Ubuntu 12.04 LTS <damir@ubuntu.(none)>
2012-12-21 14:18:10 +0100

    slider fe fix

--- application/modules/banners/views/slider_view.php
+++ application/modules/banners/views/slider_view.php
@@ -1,26 +1,25 @@
 <div class="headboxContainer">
 <div class="headbox">
-
-
-
-<div id="headboxSlider" class="royalSlider heroSlider rsMinW">
-  <?php foreach ( $items as $item ): ?>
-   <div class="rsContent">
-    <img class="rsImg" src="<?php echo fiximgsrc( $item->file_location ); ?>" alt="<?php echo $item->name; ?>" />
-    <div class="infoBlock infoBlockLeftBlack rsABlock rsNoDrag" data-fade-effect="" data-move-offset="10" data-move-effect="bottom" data-speed="200">
-       <h4><?php echo $item->name; ?></h4>
-      <p><?php echo $item->description; ?></p>
-      
- 	</div>
-    <div data-delay="350" class="bigButton rsNoDrag rsAbsoluteEl" data-fade-effect="" data-move-offset="10" data-move-effect="bottom" data-speed="200">
-        <a href="#">Više informacija</a>
+    <div id="headboxSlider" class="royalSlider heroSlider rsMinW">
+      <?php foreach ( $items as $item ): ?>
+       <div class="rsContent">
+            <img class="rsImg" src="<?php echo fiximgsrc( $item->file_location ); ?>" alt="<?php echo $item->name; ?>" />
+            <div class="infoBlock infoBlockLeftBlack rsABlock rsNoDrag" data-fade-effect="" data-move-offset="10" data-move-effect="bottom" data-speed="200">
+               <?php if( isset($module_params['title']) and $module_params['title'] == 1 ): ?>
+                <h4><?php echo $item->name; ?></h4>
+                <?php  endif; ?>
+              <?php if( isset($module_params['description']) and $module_params['description'] == 1 ): ?>  
+              <p><?php echo $item->description; ?></p>
+              <?php  endif; ?>
+            </div>
+            <div data-delay="350" class="bigButton rsNoDrag rsAbsoluteEl" data-fade-effect="" data-move-offset="10" data-move-effect="bottom" data-speed="200">
+                <a href="<?php echo $item->url; ?>">Više informacija</a>
+            </div>
+       </div>
+       <?php endforeach; ?>
+        
     </div>
-   </div>
-   <?php endforeach; ?>
-<img src="http://digitalizacija.ghetto.rs/application/views/images/slider-linija.png" width="960" height="127" style="position: absolute; bottom: 0; z-index: 110;" />
-
-
-</div>
+    <img src="http://digitalizacija.ghetto.rs/application/views/images/slider-linija.png" width="960" height="127" style="position: absolute; bottom: 0; z-index: 110;" />
          <div class="headboxBottom">Potpuno gašenje analognog servisa izvršiće se do 17. juna 2015. godine.<br />
 			<strong>Da li ste spremni za prelazak na digitalnu televiziju?</strong>
          
