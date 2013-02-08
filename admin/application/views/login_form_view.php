68288097a89748f46d988e2be121e547a216b78f
Senad Mehic <hladnopivo87@gmail.com>
2013-02-08 22:00:00 +0100

    login - changed css path

--- admin/application/views/login_form_view.php
+++ admin/application/views/login_form_view.php
@@ -3,7 +3,7 @@
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>4M Login</title>
-<link href="<?php echo base_url(); ?>application/views/css/style.css" rel="stylesheet" type="text/css" /> 
+<link href="<?php echo base_url(); ?>application/css/style.css" rel="stylesheet" type="text/css" /> 
 </head>
 <body>
 
