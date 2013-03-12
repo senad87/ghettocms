9161c042134e968fb70a7bb69abdd543887ea33d
Manson <milan@ghetto.rs>
2013-03-12 19:02:10 +0100

    last changes on loadEdit_view and css for gallery module

--- admin/application/modules/gallery/views/loadEdit_view.php
+++ admin/application/modules/gallery/views/loadEdit_view.php
@@ -1,14 +1,10 @@
-Title:
-<input class="instant-update" id="title" name="image-title" type="text" value="<?php if(count($images) == 1 && $images[0]['title']){ echo $images[0]['title'];} ?>" /><br />
+<div class="descView">
+<label>Title:</label><br />
+<input class="instant-update lform2" id="title" name="image-title" type="text" value="<?php if(count($images) == 1 && $images[0]['title']){ echo $images[0]['title'];} ?>" /><br />
 
 
-Lead:
-<textarea class="instant-update" id="lead" name="image-lead"><?php if(count($images) == 1 && $images[0]['lead']){ echo $images[0]['lead'];} ?></textarea>
-
-
-
-
-<!--<a id="update" href="#" >Update</a>-->
+<label>Lead:</label><br />
+<textarea class="instant-update lform2-textarea" id="lead" name="image-lead"><?php if(count($images) == 1 && $images[0]['lead']){ echo $images[0]['lead'];} ?></textarea>
 
 <input id="ids" type="hidden" value=<?php echo $ids; ?> />
-
+</div>
